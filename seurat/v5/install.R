898d66fb5167f86e51055aba249886a07684e231
joan-yanqiong <Joan.Kant@uhn.ca>
2025-04-09 17:23:32 -0400

    add SeuratWrappers & colorJam to Seurat v5 image

--- seurat/v5/install.R
+++ seurat/v5/install.R
@@ -1,6 +1,6 @@
 options(repos = "https://CRAN.R-project.org")
 
 message("Install packages with pak...")
-pak::pkg_install(c("GaitiLab/GaitiLabUtils", "mojaveazure/seurat-disk"))
+pak::pkg_install(c("GaitiLab/GaitiLabUtils", "mojaveazure/seurat-disk", "satijalab/seurat-wrappers", "jmw86069/colorjam@fe1658de43da284e2d40c3f049fe9334caec4f19"))
 
 message("Finished!")
